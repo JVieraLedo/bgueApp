3f3ced55a9f1b60bf18ed94817bad17f8d759fb2
XE22602 <jose.viera@grupoonetec.com>
2016-10-24 17:13:58 +0200

    add all files

--- appbgue/www/scripts/controllers/main.controller.js
+++ appbgue/www/scripts/controllers/main.controller.js
@@ -5,17 +5,13 @@
         .controller('MainController', MainController);
 
 
-    MainController.$inject = ['$state', 'dataService'];
+    MainController.$inject = ['$state'];
 
-    function MainController($state, dataService) {
+    function MainController($state) {
         var vm = this;
-
         vm.continue = function () {
             $state.go('track1');
-            var now = new Date().getTime() / 1000;
-            dataService.loginApp(parseInt(now));
         };
-
     }
 
 })(window.angular);
